aff222899667d2d000b82f0da54f59163e407d4f
pavan boina <pavanboina6@gmail.com>
2019-12-28 06:21:59 +0000

    Added files

---
+++ TASK-12/code.py
@@ -0,0 +1,32 @@
+import string
+
+
+def xor(x):
+    return ''.join(chr(ord(i)^10) for i in x)
+
+
+def shift(x):
+    upper = string.ascii_uppercase
+    lower = string.ascii_lowercase
+    digits = string.digits
+    n = []
+    for i in x:
+        if i.isupper() is True:
+            n.append(upper[(upper.index(i)-3)%26])
+        elif i.islower() is True:
+            n.append(lower[(lower.index(i)-3)%26])
+        elif i.isdigit() is True:
+            n.append(digits[(digits.index(i)-3)%10])
+    return n
+
+def decode(x):
+    return x.decode("hex")
+
+if __name__=="__main__":
+    print "Decoded output: "
+
+    arr = "667b6c7d63677f3c733c7f323c3c7b333e7b3c3c7f3e7b333e3232393c3d3268"
+
+    your_code = shift(xor(decode(arr)))
+
+    print your_code
